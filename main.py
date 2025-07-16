3d4460e6be59cf90138368dabf85396cfd0ba49f
Ericka Lewis <157831388+Lewise55@users.noreply.github.com>
2025-07-16 02:47:52 +0000

    updated live chat room

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ app.config["SECRET_KEY"] = "bhbghewbgcfuagbu"
 socketio = SocketIO(app)
 
 rooms = {}
-def generate_unique_code(Length):
+def generate_unique_code(length):
     while True:
         code = ""
         for _ in range(length):
@@ -32,14 +32,14 @@ def home():
             return render_template("home.html", error="Please enter a name.", code=code, name=name)
 
         if join != False and not code:  
-            return render_template("home.html", error="Please enter a room code.", error="Please enter a name.", code=code, name=name)
+            return render_template("home.html", error="Please enter a room code.", code=code, name=name)
 
         room = code
         if create != False:
             room = generate_unique_code(4)
             rooms[room] = {"members": 0, "messages": []}
         elif code not in rooms:
-            return render_template("home.html", error="Room does not exist", error="Please enter a name.", code=code, name=name)
+            return render_template("home.html", error="Room does not exist", code=code, name=name)
 
 
         session["room"] = room
@@ -50,7 +50,54 @@ def home():
 
 @app.route("/room")
 def room():
-    return render_template("room.html")
+    room = session.get("room")
+    if room is None or session.get("name") is None or room not in rooms:
+        return redirect(url_for("home"))
+
+    return render_template("room.html", code=room, messages=rooms[room]["messages"])
+
+@socketio.on("message")
+def message(data):
+    room = session.get("room")
+    if room not in rooms:
+        return
+
+    content = {
+        "name": session.get("name"),
+        "message": data["data"]
+    }
+    send(content, to=room)
+    rooms[room]["messages"].append(content)
+    print(f"{session.get('name')} said: {data['data']}")
+
+@socketio.on("connect")
+def connect(auth):
+    room = session.get("room")
+    name = session.get("name")
+    if not room or not name:
+        return
+    if room not in rooms:
+        leave_room(room)
+        return
+
+    join_room(room)
+    send({"name": name, "message": "has entered the room"}, to=room)
+    rooms[room]["members"] += 1
+    print(f"{name} joined room {room}")
+
+@socketio.on("disconnect")
+def disconnect():
+    room = session.get("room")
+    name = session.get("name")
+    
+    leave_room(room)
+    if room in rooms:
+        rooms[room]["members"] -= 1
+        if rooms[room]["members"] <= 0:
+            del rooms[room]
+
+    send({"name": name, "message": "has left the room"}, to=room)
+    print(f"{name} has left room {room}")
 
 if __name__ == "__main__":
     socketio.run(app, debug=True)
